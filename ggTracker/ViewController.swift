d2b539a0b468f1558f08362ca22051e968893971
Siobahn Devlin <sdevlin2021@fau.edu>
2022-10-15 21:23:47 -0400

    SD-Initial Commit

---
+++ ggTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ggTracker
+//
+//  Created by Siobahn Devlin on 10/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
