f8991f49149d82f29c2110bfc72f98868f0bae70
Paulina D <pdevito1188@gmail.com>
2022-10-31 21:47:49 -0400

    Starting to set up the UI for the home

---
+++ ggTracker/HomeCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeCell.swift
+//  ggTracker
+//
+//  Created by Paulina DeVito on 10/31/22.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
