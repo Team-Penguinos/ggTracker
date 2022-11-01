a5a9f1a545aaedc0d26502f8ee2b95dd246c5c52
Kirk Shields <35874607+kshields2012@users.noreply.github.com>
2022-10-31 21:19:48 -0400

    Details Screen Layout
    Implemented the layout for the DiscoverViewController

---
+++ ggTracker/DiscoverViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiscoverViewController.swift
+//  ggTracker
+//
+//  Created by Kirk Shields on 10/31/22.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
